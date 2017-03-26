d8c311cbc45e7df60ac01bb25c75f3160116df69
Brayden <bhollis13@my.whitworth.edu>
2017-03-26 17:36:20 -0400

    added comments about how this should take a functor to scale delta z according to sensor type (address nonlinearities)

--- sensor/readSensor.m
+++ sensor/readSensor.m
@@ -1,5 +1,10 @@
 function values = readSensor(sensor)
 
+%NOTE: Should sensor read delta-z or the force (i.e. should multiply by
+%spring constant)?
 values = sensor.MAXZ-sensor.taxels(:,3);
+
+%NOTE: Realistically, should make it so one could apply only reading
+%function, such as implement nonlinear readings.
 
 end
