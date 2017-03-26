83921ffe8f6b046f5928bdfab808f084e373fb26
Brayden <bhollis13@my.whitworth.edu>
2017-03-26 17:40:40 -0400

    not sure

--- object/mesh/defaultDrill.m
+++ object/mesh/defaultDrill.m
@@ -6,4 +6,5 @@ drill = centerObj(drill);
 drill = setObjPosition(drill,[0;0;.0532]);
 drill = setObjOrientation(drill,kth2R([0,1,0],pi/2)*kth2R([0,0,1],pi/4+pi/16));
 drill = setObjMass(drill,.858);
+drill = computeObjInertiaTensor(drill);
 end
--- object/updateObjectQS.m
+++ object/updateObjectQS.m
@@ -12,7 +12,7 @@ torque = [0;0;0];
 
 global world
 
-%compute force and torque from world (eg gravity)
+%compute force and torque from world (eg gravity) (in object frame)
 worldForce = computeWorldForce(object);
 %TODO: any world torques (gravity does not have one)
 
@@ -20,7 +20,7 @@ if ~isempty(worldForce)
     force = force + worldForce;
 end
 
-%compute force and torque from sensors)
+%compute force and torque from sensors) (in object frame)
 for sen = 1:length(world.sensors)
     %compute from sensor
     [sensorForce,locations] = computeSensorForce(object,world.sensors{sen});
@@ -42,7 +42,8 @@ end
 %TODO: torque
 
 
-%compute external force and torque (a.k.a., planned' force)
+%compute external force and torque (a.k.a., planned' force) (in object
+%frame) (plans are stored in world frame)
 %TODO: force
 %verify step of the plan.
 if object.planStep < size(object.plan,1) && currentStep*stepSize >= object.plan(object.planStep+1,1)
@@ -59,13 +60,23 @@ end
 % object.velocity = object.qsForceConstant*object.orientation*force + world.damper*object.velocity;
 % object.angularVelocity = object.orientation*object.qsTorqueConstant*torque + world.damper*object.angularVelocity;
 %MemoryLess
+%object velocity is in world frame
 object.velocity = object.qsForceConstant*object.orientation*force;
-object.angularVelocity = object.orientation*object.qsTorqueConstant*torque;
+% object.angularVelocity = object.orientation*object.qsTorqueConstant*torque;
+object.angularVelocity = object.qsTorqueConstant*torque; %in body frame
+object.angularVelocity = torque; %in body frame
 
 %update position
+%object position and orientation are in world frame
 object.position = object.position + object.velocity*stepSize;
 %update orientation
-object.orientation = object.orientation + matcross(object.angularVelocity)*object.orientation*stepSize;
+% object.orientation = object.orientation + matcross(object.angularVelocity)*object.orientation*stepSize;
+if norm(object.angularVelocity) > 0
+    object.orientation = object.orientation * kth2R(object.angularVelocity/norm(object.angularVelocity),...
+        norm(object.angularVelocity)*stepSize);
+% object.orientation = kth2R(object.angularVelocity/norm(object.angularVelocity),...
+%         norm(object.angularVelocity)*stepSize) * object.orientation;
+end
 
 % disp('torque:')
 % disp(torque);
