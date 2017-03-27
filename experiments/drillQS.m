54d0177111dcc68bb6fdab4588c02bbfe3f421d4
Brayden <bhollis13@my.whitworth.edu>
2017-03-27 12:08:21 -0400

    random changes, but may need when talk to Jeff

--- experiments/drillQS.m
+++ experiments/drillQS.m
@@ -1,10 +1,11 @@
-filename = 'drillQSWithHalfMemoryg9_8';
+filename = 'drill_test';
 
 numSteps = 100; timestep = 0.01;
 initWorld(numSteps,timestep);
 setWorldDamper(0);
+setWorldGraphicsResolution(5);
 setGravity([0;0;-9.8]);
-% setRecordOn(filename);
+setRecordOn(filename);
 setRecordOff
 
 object = initPLYMeshObject('drill.ply');
@@ -14,6 +15,10 @@ object = setObjOrientation(object,kth2R([0,1,0],pi/2)*kth2R([0,0,1],pi/4+pi/16))
 object = setObjMass(object,.858);
 object = computeObjInertiaTensor(object);
 object = computeObjQSConstants(object,timestep);
+% object = defaultDrill;
+% object = setObjPosition(object,[0;0;.06]);
+% object = computeObjInertiaTensor(object);
+% object = computeObjQSConstants(object,timestep);
 addObject(object);
 
 sensor = initRectangularSensor(64,64,.0025,0,.02);
